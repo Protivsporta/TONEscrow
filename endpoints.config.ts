ac12b02a122768c9f40be05267a248a469b1f950
Alexander Bushukin <alexandr.bushukin@gmail.com>
2022-11-17 01:28:53 +0400

    Changed gitignore

---
+++ endpoints.config.ts
@@ -0,0 +1,14 @@
+export default {
+    MNEMONIC: [
+        'entire',  'consider', 'anchor',
+        'artist',  'cruel',    'must',
+        'common',  'mosquito', 'stamp',
+        'wide',    'flag',     'gift',
+        'board',   'library',  'anchor',
+        'beyond',  'select',   'burst',
+        'exile',   'tomorrow', 'soul',
+        'alcohol', 'forward',  'first'
+      ] ,
+    API_KEY: '41024ba3a6c66ef288d64dd570849e4a9b68fe9e21a69aa52e23cc20d6a1f12d',
+    TG_TOKEN: '5444129713:AAHw1PkWEQ6qUYfKrOmrA6hLBMoyfTRBlS0'
+}
